3231a64d583b05f59ff030670ed8e5b839831881
phucpham <phamhuuphuc4049@gmail.com>
2024-06-13 23:39:46 +0700

    Xong landingpage

--- frontend/src/pages/start/start.js
+++ frontend/src/pages/start/start.js
@@ -1,13 +1,41 @@
 import { Container, Row, Col, Button, Form, Alert, Modal, Dropdown } from 'react-bootstrap';
-import React, { useState, useEffect } from 'react';
-import globalStyles from '../../globalStyles.module.css'
-import styles from './start.module.css'
-
+import React, { useState, useEffect} from 'react';
+import { useNavigate } from 'react-router-dom';
+import './start.css'
+import CustomButton from '../../components/CustomButton';
 export const Start = () => {
-
+    const navigate = useNavigate();
+    const handleButtonPress = () => {
+        navigate('/login');
+    };
     return (
-        <div>
-            ưeewr
-        </div>
-    );
+            <div className="container">
+              <div className="hero">
+                <img
+                  src={require('../../../assets/img/landingPage.png')}
+                  className="heroImage"
+                  alt="Hero"
+                />
+              </div>
+              <div className="content">
+                <div className="contentHeader">
+                  <h1 className="title">
+                    Làm việc hiệu quả hơn <br /> cùng{' '}
+                    <span className="appName">
+                      <span className="appNameText">EduKpi</span>
+                    </span>
+                  </h1>
+                  <p className="text">
+                    Chúng tôi tự tin mang lại cho bạn hiệu suất làm việc vượt trội
+                  </p>
+                </div>
+        
+                <CustomButton
+                  title="Bắt đầu ngay"
+                  handlePress={handleButtonPress }
+                  containerStyles="w-full mt-7"
+                />
+              </div>
+            </div>
+          );
 };
